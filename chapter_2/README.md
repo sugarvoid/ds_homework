4f0b49af6fc0621756b7024053669be186666c08
Dave <48969490+sugarvoid@users.noreply.github.com>
2025-01-22 18:28:16 -0600

    Make variable names better

--- chapter_2/README.md
+++ chapter_2/README.md
@@ -1,21 +1,16 @@
 
 # Chapter 2 Assignment
 
-I choose to do projects 2.1, 2.2, 2.4
+I choose to do projects 2.1, 2.2, 2.4 and 2.5
 
 
 ## Array class
 
-### Changed variable names to be more clear.
-### Added a more detailed error when trying to call `insert()` when the array if already full.
-```python
-     def insert(self, item: Any) -> None:
-        if self.__item_limit == self.__item_count:
-            raise Exception(f"Failed to insert {item}. Array has reached its limit") 
-        self.__list[self.__item_count] = item  
-        self.__item_count += 1 
-```
-### Added `get_max_num()` that returns the value of the highest number in the array, or None if the array has no numbers. (Project 2.1)
-### Added `delete_max_num()` that returns the value of the highest number in the array, or None if the array has no numbers. But also removes that number from the array. (Project 2.2)
-### Added `remove_dupes()` that removes any duplicate entries in the array. (Project 2.4)
-### Added `replace()` that replaces the array with a new array, since so far 2 projects required the array be modified and this made it easier since the internal list and the item_count had to be updated if time.
+### Changed 
+- Variable names to be more clear.
+### Added 
+- More detailed error when trying to call `insert()` when the array if already full.
+- `get_max_num()` that returns the value of the highest number in the array, or None if the array has no numbers. (Project 2.1)
+- `delete_max_num()` that returns the value of the highest number in the array, or None if the array has no numbers. But also removes that number from the array. **(Project 2.2)**
+- `remove_dupes()` that removes any duplicate entries in the array. **(Project 2.4)**
+- `replace()` that replaces the array with a new array, since so far 2 projects required the array be modified and this made it easier since the internal list and the item_count had to be updated if time.
--- chapter_2/classes.py
+++ chapter_2/classes.py
@@ -115,39 +115,39 @@ class Array(object):
 # Implement an Ordered Array of Records structure
 class OrderedRecordArray(object):
     def __init__(self, initialSize, key=identity):  # Constructor
-        self.__a = [None] * initialSize  # The array stored as a list
-        self.__nItems = 0  # No items in array initially
+        self.__list = [None] * initialSize  # The array stored as a list
+        self.__item_count = 0  # No items in array initially
         self.__key = key  # Key function gets record key
 
     def __len__(self):  # Special def for len() func
-        return self.__nItems  # Return number of items
+        return self.__item_count  # Return number of items
 
     def get(self, n: int):  # Return the value at index n
-        if n >= 0 and n < self.__nItems:  # Check if n is in bounds, and
-            return self.__a[n]  # only return item if in bounds
+        if n >= 0 and n < self.__item_count:  # Check if n is in bounds, and
+            return self.__list[n]  # only return item if in bounds
         raise IndexError("Index " + str(n) + " is out of range")
 
     def traverse(self, function: Callable = print):  # Traverse all items
-        for j in range(self.__nItems):  # and apply a function
-            function(self.__a[j])
+        for j in range(self.__item_count):  # and apply a function
+            function(self.__list[j])
 
     def __str__(self):  # Special def for str() func
-        ans = "["  # Surround with square brackets
-        for i in range(self.__nItems):  # Loop through items
-            if len(ans) > 1:  # Except next to left bracket,
-                ans += ", "  # separate items with comma
-            ans += str(self.__a[i])  # Add string form of item
-        ans += "]"  # Close with right bracket
-        return ans
+        output_str = "["  # Surround with square brackets
+        for i in range(self.__item_count):  # Loop through items
+            if len(output_str) > 1:  # Except next to left bracket,
+                output_str += ", "  # separate items with comma
+            output_str += str(self.__list[i])  # Add string form of item
+        output_str += "]"  # Close with right bracket
+        return output_str
 
     def find(self, key):  # Find index at or just below key
         lo = 0  # in ordered list
-        hi = self.__nItems - 1  # Look between lo and hi
+        hi = self.__item_count - 1  # Look between lo and hi
         while lo <= hi:
             mid = (lo + hi) // 2  # Select the midpoint
-        if self.__key(self.__a[mid]) == key:  # Did we find it?
+        if self.__key(self.__list[mid]) == key:  # Did we find it?
             return mid  # Return location of item
-        elif self.__key(self.__a[mid]) < key:  # Is key in upper half?
+        elif self.__key(self.__list[mid]) < key:  # Is key in upper half?
             lo = mid + 1  # Yes, raise the lo boundary
         else:
             hi = mid - 1  # No, but could be in lower half
@@ -155,63 +155,63 @@ class OrderedRecordArray(object):
 
     def search(self, key):
         idx = self.find(key)  # Search for a record by its key
-        if idx < self.__nItems and self.__key(self.__a[idx]) == key:
-            return self.__a[idx]  # and return item if found
+        if idx < self.__item_count and self.__key(self.__list[idx]) == key:
+            return self.__list[idx]  # and return item if found
 
     def insert(self, item: Any):  # Insert item into the correct position
-        if self.__nItems >= len(self.__a):  # If array is full,
+        if self.__item_count >= len(self.__list):  # If array is full,
             raise Exception("Array overflow")  # raise exception
         j = self.find(self.__key(item))  # Find where item should go
-        for k in range(self.__nItems, j, -1):  # Move bigger items right
-            self.__a[k] = self.__a[k - 1]
-        self.__a[j] = item  # Insert the item
-        self.__nItems += 1  # Increment the number of items
+        for k in range(self.__item_count, j, -1):  # Move bigger items right
+            self.__list[k] = self.__list[k - 1]
+        self.__list[j] = item  # Insert the item
+        self.__item_count += 1  # Increment the number of items
 
     def delete(self, item: Any):  # Delete any occurrence
         j = self.find(self.__key(item))  # Try to find the item
-        if j < self.__nItems and self.__a[j] == item:  # If found,
-            self.__nItems -= 1  # One fewer at end
-            for k in range(j, self.__nItems):  # Move bigger items left
-                self.__a[k] = self.__a[k + 1]
-        return True  # Return success flag
+        if j < self.__item_count and self.__list[j] == item:  # If found,
+            self.__item_count -= 1  # One fewer at end
+            for k in range(j, self.__item_count):  # Move bigger items left
+                self.__list[k] = self.__list[k + 1]
+            return True  # Return success flag
 
         return False  # Made it here; item not found
 
 
 class OrderedArray(object):
-    def __init__(self, initialSize: int):  # Constructor
-        self.__a = [None] * initialSize  # The array stored as a list
-        self.__nItems = 0  # No items in array initially
+    def __init__(self, initial_size: int):  # Constructor
+        self.__list = [None] * initial_size  # The array stored as a list
+        self.__item_count = 0  # No items in array initially
 
     def __len__(self):  # Special def for len() func
-        return self.__nItems  # Return number of items
+        return self.__item_count  # Return number of items
 
     def get(self, n):  # Return the value at index n
-        if 0 <= n and n < self.__nItems:  # Check if n is in bounds,
-            return self.__a[n]  # only return item if in bounds
+        if 0 <= n and n < self.__item_count:  # Check if n is in bounds,
+            return self.__list[n]  # only return item if in bounds
         raise IndexError("Index " + str(n) + " is out of range")
 
     def traverse(self, function=print):  # Traverse all items and apply a function
-        for j in range(self.__nItems):  # Loop through items
-            function(self.__a[j])
+        for j in range(self.__item_count):  # Loop through items
+            function(self.__list[j])
 
     def __str__(self):  # Special def for str() func
-        ans = "["  # Surround with square brackets
-        for i in range(self.__nItems):  # Loop through items
-            if len(ans) > 1:  # Except next to left bracket,
-                ans += ", "  # separate items with a comma
-            ans += str(self.__a[i])  # Add string form of item
-        ans += "]"  # Close with right bracket
-        return ans
+        output_str = "["  # Surround with square brackets
+        for i in range(self.__item_count):  # Loop through items
+            if len(output_str) > 1:  # Except next to left bracket,
+                output_str += ", "  # separate items with a comma
+            output_str += str(self.__list[i])  # Add string form of item
+        output_str += "]"  # Close with right bracket
+        return output_str
 
     def find(self, item):  # Find index at or just below item in ordered list
         lo = 0  # Look between lo and hi
-        hi = self.__nItems - 1
+        hi = self.__item_count - 1
         while lo <= hi:
             mid = (lo + hi) // 2  # Select the midpoint
-            if self.__a[mid] == item:  # Did we find it at midpoint?
+            if self.__list[mid] == item:  # Did we find it at midpoint?
                 return mid  # Return location of item
-            elif self.__a[mid] < item:  # Is item in upper half?
+            elif self.__list[mid] < item:  # Is item in upper half?
                 lo = mid + 1  # Yes, raise the lo boundary
             else:
                 hi = mid - 1  # No, but could be in lower half
@@ -219,23 +219,23 @@ class OrderedArray(object):
 
     def search(self, item):  # Search for item
         index = self.find(item)  # Find the index where item should go
-        if index < self.__nItems and self.__a[index] == item:
-            return self.__a[index]  # Return item if found
+        if index < self.__item_count and self.__list[index] == item:
+            return self.__list[index]  # Return item if found
 
     def insert(self, item):  # Insert item into correct position
-        if self.__nItems >= len(self.__a):  # If array is full,
+        if self.__item_count >= len(self.__list):  # If array is full,
             raise Exception("Array overflow")  # raise exception
         index = self.find(item)  # Find index where item should go
-        for j in range(self.__nItems, index, -1):  # Move bigger items to the right
-            self.__a[j] = self.__a[j - 1]  # Shift item
-        self.__a[index] = item  # Insert the item
-        self.__nItems += 1  # Increment the number of items
+        for j in range(self.__item_count, index, -1):  # Move bigger items to the right
+            self.__list[j] = self.__list[j - 1]  # Shift item
+        self.__list[index] = item  # Insert the item
+        self.__item_count += 1  # Increment the number of items
 
     def delete(self, item):  # Delete any occurrence of item
         j = self.find(item)  # Try to find the item
-        if j < self.__nItems and self.__a[j] == item:  # If found
-            self.__nItems -= 1  # One fewer at the end
-            for k in range(j, self.__nItems):  # Move bigger items left
-                self.__a[k] = self.__a[k + 1]
+        if j < self.__item_count and self.__list[j] == item:  # If found
+            self.__item_count -= 1  # One fewer at the end
+            for k in range(j, self.__item_count):  # Move bigger items left
+                self.__list[k] = self.__list[k + 1]
             return True  # Return success flag
         return False  # Item not found
