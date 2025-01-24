14d1b840517dfb5908266895c45a3111ac2d5eb8
Dave <48969490+sugarvoid@users.noreply.github.com>
2025-01-24 15:49:23 -0600

    Update naming convention

--- chapter_2/homework.py
+++ chapter_2/homework.py
@@ -1,3 +1,7 @@
+# David Durham
+# Week 1
+# Project 2.7
+
 from classes import Array, OrderedRecordArray
 from random import choice, randint
 from string import ascii_letters
@@ -12,79 +16,81 @@ def test_function():
 
 
 def project_2_1():
-    array = Array(100)
-    array.insert(77)
-    array.insert(99)
-    array.insert(test_function)
-    array.insert(100)
-    array.insert(-49)
-    array.insert("alpha")
-    array.insert("beta")
-    array.insert("charlie")
-    array.insert("delta")
+    _array = Array(100)
+    _array.insert(77)
+    _array.insert(99)
+    _array.insert(test_function)
+    _array.insert(100)
+    _array.insert(-49)
+    _array.insert("alpha")
+    _array.insert("beta")
+    _array.insert("charlie")
+    _array.insert("delta")
 
-    print(f"The max numeric value is: {array.get_max_num()}")
+    print(f"The max numeric value is: {_array.get_max_num()}")
 
 
 def project_2_2():
-    array = Array(100)
-    array.insert(77)
-    array.insert(99)
-    array.insert(100)
-    array.insert(-49)
-    array.insert("alpha")
-    array.insert("beta")
-    array.insert("charlie")
-    array.insert("delta")
+    _array = Array(100)
+    _array.insert(77)
+    _array.insert(99)
+    _array.insert(100)
+    _array.insert(-49)
+    _array.insert("alpha")
+    _array.insert("beta")
+    _array.insert("charlie")
+    _array.insert("delta")
 
     print("Before delete_max_num()")
-    print(f"Array size: {len(array)}. Highest numeric value: {array.get_max_num()}")
-    print(array.delete_max_num())
+    print(f"Array size: {len(_array)}. Highest numeric value: {_array.get_max_num()}")
+    print(_array.delete_max_num())
 
     print("After delete_max_num()")
-    print(f"Array size: {len(array)}. Highest numeric value: {array.get_max_num()}")
+    print(f"Array size: {len(_array)}. Highest numeric value: {_array.get_max_num()}")
 
 
 def project_2_4():
-    array = Array(100)
-    array.insert(77)
-    array.insert(77)
-    array.insert(77)
-    array.insert(77)
-    array.insert(99)
-    array.insert(100)
-    array.insert(-49)
-    array.insert("alpha")
-    array.insert("beta")
-    array.insert("charlie")
-    array.insert("delta")
-    array.insert("delta")
-    array.insert("delta")
-    array.insert("delta")
+    _array = Array(100)
+    _array.insert(77)
+    _array.insert(77)
+    _array.insert(77)
+    _array.insert(77)
+    _array.insert(99)
+    _array.insert(100)
+    _array.insert(-49)
+    _array.insert("alpha")
+    _array.insert("beta")
+    _array.insert("charlie")
+    _array.insert("delta")
+    _array.insert("delta")
+    _array.insert("delta")
+    _array.insert("delta")
 
     print("Array before remove_dupes()")
-    print(len(array))
+    print(len(_array))
     # array.traverse()
-    array.remove_dupes()
+    _array.remove_dupes()
     print("Array after remove_dupes()")
-    print(len(array))
+    print(len(_array))
     # array.traverse()
 
 
 def project_2_7():
-    maxSize = 5
-    arr = OrderedRecordArray(maxSize, second, resizable=True)
+    # Create array and set array size to 5
+    _array = OrderedRecordArray(5, second, resizable=True)
 
+    # Add random data to arrow
     for _ in range(5):
         _random_letter = choice(ascii_letters)
         _random_int = randint(1, 50)
-        arr.insert((_random_letter, _random_int))
+        _array.insert((_random_letter, _random_int))
 
-    print(f"Array's size: {arr.get_size()}")
-    print("Array containing", len(arr), "items:\n", arr)
-    arr.insert(("new", 8))
-    print("Array after insert has", len(arr), "items:\n", arr)
-    print(f"Array's size: {arr.get_size()}")
+
+    print(f"Array's size: {_array.get_size()}")
+    print("Array containing", len(_array), "items:\n", _array)
+    _array.insert(("new", 8))
+    print("Array after insert has", len(_array), "items:\n", _array)
+    print(f"Array's size: {_array.get_size()}")
 
 
 if __name__ == "__main__":
